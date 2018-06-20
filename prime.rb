842e83169f194a0a9de7e1df3af92858ab87e8a0
Maduri Vassaramo <maduri.vassaramo@hotmail.co.uk>
2018-06-20 18:50:59 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,22 @@
 def prime?(n)
+<<<<<<< HEAD
   if n == 0 or n == 1 or n < 0
+=======
+  if n == 0 or n == 1 
+>>>>>>> abfe7c15f1f1198f2c925399c94614fd62fbf290
     return false 
   end  
   k = 2 
   while k <= Math.sqrt(n)
     if n % k == 0 
       return false 
+<<<<<<< HEAD
     end
      k += 1
+=======
+      k += 1 
+    end
+>>>>>>> abfe7c15f1f1198f2c925399c94614fd62fbf290
   end 
   return true 
 end
