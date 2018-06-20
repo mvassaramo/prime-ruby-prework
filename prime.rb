8e597c1c129b9bc473d83ce83f7c9bac7946bef2
Maduri Vassaramo <maduri.vassaramo@hotmail.co.uk>
2018-06-20 18:49:57 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(n)
+  if n == 0 or n == 1 or n < 0
+    return false 
+  end  
+  k = 2 
+  while k <= Math.sqrt(n)
+    if n % k == 0 
+      return false 
+    end
+     k += 1
+  end 
+  return true 
+end
